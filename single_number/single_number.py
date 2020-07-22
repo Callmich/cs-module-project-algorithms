47da03ebd462acbf0a8af9f097f8194ea0b5e292
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-22 15:45:08 -0600

    single number is now passing test

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,23 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+def single_number(arr):
+
+    dic = {}
+    for x in arr:
+        if dic.get(x):
+            dic[x] = 2
+        else:
+            dic[x] = 1
+    key_list = list(dic.keys())
+    val_list = list(dic.values())
+    
+    return key_list[val_list.index(1)]
+    
+
+
+    
 
 
 if __name__ == '__main__':
