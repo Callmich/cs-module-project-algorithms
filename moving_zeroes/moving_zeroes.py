f718acf23f46a23972e06da3e5badbe410b8c017
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-22 16:19:16 -0600

    moving zeroes is now passing test

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,31 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    # itterate through the array 
+        # if n == 0 
+            # remove that from the array
+            # add to a 0 counter
+        # else
+            # add to a number counter
+        # add a 0 to the end of the array for each 0
+        # return array and possibly number counter
 
-    pass
+    zeroCounter = 0
+    numberCounter = 0
+    zeroArr = []
+    for x in range(len(arr)):
+        if arr[x] == 0:
+            zeroArr.append(x)
+            zeroCounter += 1
+    zeroArr = zeroArr[::-1]
+    for x in zeroArr:
+        del arr[x]
+    for x in range(zeroCounter):
+        arr.append(0)
+    
+    return arr
+
+
 
 
 if __name__ == '__main__':
