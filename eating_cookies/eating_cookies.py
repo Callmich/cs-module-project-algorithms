d962ec1549113c1e2dd4cc3e484845e84ea9d8ad
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-22 19:32:57 -0600

    first attempt is getting the correct answer with 3 cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,23 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    if n < 0:
+        return 0
+    if n < 2:
+        return 1
+    else:
+        currentCookie = 0
+        if currentCookie > n:
+            return 0
+        if currentCookie == n:
+            return 1
+        else:
+            return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
-    pass
+
+# if there is less than 2 cookies there is only one way to eat it
+# I want to set a counter and update it by one two or three recursively until the counter either hits n or goes over it
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
